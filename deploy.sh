55fb601645616b7d14a82b567342b5dee6de0cc6
Julian Tekook <julian@tekook.de>
2019-12-13 16:46:01 +0100

    add sqlsrv image, but disable for now.

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --push )           PUSH_TO_REGISTRY=1
+                                ;;
+    esac
+    shift
+done
+
+export PUBLISHER=tekook
+export IMAGES=("laravel-fpm")
 export PHP_VERSION=("7.3.12" "7.4.0")
-export IMAGE=tekook/laravel-fpm
 
-for version in ${PHP_VERSION[@]}; do
-    echo Building php-version: $version
-    short_version=${version%.*}
+for image in ${IMAGES[@]}; do
+    dockerFile=./${image//-//}/Dockerfile
+    imageName=${PUBLISHER}/${image}
+    echo Building ${imageName} from ${dockerFile}
+    for version in ${PHP_VERSION[@]}; do
+        echo Building php-version: $version
+        short_version=${version%.*}
 
-    docker build . -f ./laravel/fpm/Dockerfile -t ${IMAGE} --build-arg PHP_VERSION=${version}
+        docker build . -f ${dockerFile} -t ${imageName} --build-arg PHP_VERSION=${version}
 
-    docker tag ${IMAGE} ${IMAGE}:${version}
-    docker tag ${IMAGE} ${IMAGE}:${short_version}
+        docker tag ${imageName} ${imageName}:${version}
+        docker tag ${imageName} ${imageName}:${short_version}
 
 
-    echo pushing: ${IMAGE}:${version} 
-    docker push ${IMAGE}:${version}
-    echo pushing: ${IMAGE}:${short_version} 
-    docker push ${IMAGE}:${short_version}
+        if [ ! -z "$PUSH_TO_REGISTRY" ]
+        then
+            echo pushing: ${imageName}:${version} 
+            docker push ${imageName}:${version}
+            echo pushing: ${imageName}:${short_version} 
+            docker push ${imageName}:${short_version}
+        fi
+    done
 done
 
 
