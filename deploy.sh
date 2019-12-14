1e509ce1c535c12c3fde5de603a15decf59f3dab
Julian Tekook <Julian@Tekook.de>
2019-12-14 16:29:20 +0100

    add image version, add skip params

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
+export PUBLISHER=tekook
+export IMAGES=("laravel-fpm")
+export IMAGE_VERSION=1.0.0
+export PHP_VERSION=("7.3.12" "7.4.0")
 
 while [ "$1" != "" ]; do
     case $1 in
         -p | --push )           PUSH_TO_REGISTRY=1
                                 ;;
-        --latest)               PUSH_LATEST=1
+        -i | --images )         IMAGES=($2)
+                                ;;
+        --publisher )           PUBLISHER=$2
+                                ;;
+        --skip-build )          SKIP_BUILD=1
+                                ;;
+        --push-latest)          PUSH_LATEST=1
                                 ;;
     esac
     shift
 done
-
-export PUBLISHER=tekook
-export IMAGES=("laravel-fpm")
-export PHP_VERSION=("7.3.12" "7.4.0")
 
 for image in ${IMAGES[@]}; do
     dockerFile=./${image//-//}/Dockerfile
@@ -23,18 +29,21 @@ for image in ${IMAGES[@]}; do
         echo Building php-version: $version
         short_version=${version%.*}
 
-        docker build . -f ${dockerFile} -t ${imageName} --build-arg PHP_VERSION=${version}
+        if [ -z "$SKIP_BUILD" ]
+        then
+           docker build . -f ${dockerFile} -t ${imageName} --build-arg PHP_VERSION=${version}
+        fi
 
-        docker tag ${imageName} ${imageName}:${version}
-        docker tag ${imageName} ${imageName}:${short_version}
+        docker tag ${imageName} ${imageName}:${IMAGE_VERSION}_${version}
+        docker tag ${imageName} ${imageName}:${IMAGE_VERSION}_${short_version}
 
 
         if [ ! -z "$PUSH_TO_REGISTRY" ]
         then
-            echo pushing: ${imageName}:${version} 
-            docker push ${imageName}:${version}
-            echo pushing: ${imageName}:${short_version} 
-            docker push ${imageName}:${short_version}
+            echo pushing: ${imageName}:${IMAGE_VERSION}_${version} 
+            docker push ${imageName}:${IMAGE_VERSION}_${version}
+            echo pushing: ${imageName}:${IMAGE_VERSION}_${short_version} 
+            docker push ${imageName}:${IMAGE_VERSION}_${short_version}
         fi
     done
     if [ ! -z "$PUSH_LATEST" ]
